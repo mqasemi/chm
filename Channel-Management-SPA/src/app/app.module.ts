447327227fa3ea83feb3b62a9143b3158cfe7aba
ms-ghasemi <ms-ghasemi@agri-bank.com>
2020-05-12 13:36:15 +0430

    admnlte added

--- Channel-Management-SPA/src/app/app.module.ts
+++ Channel-Management-SPA/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppheaderComponent } from './appheader/appheader.component';
+import { AppmenuComponent } from './appmenu/appmenu.component';
+import { AppfooterComponent } from './appfooter/appfooter.component';
+import { AppsettingComponent } from './appsetting/appsetting.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppheaderComponent
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    AppsettingComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
